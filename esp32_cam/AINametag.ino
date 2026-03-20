13dcb4cfebe698de01af54f78cbe66bbe91831ef
GraydonCharlesCory <GraydonCharlesCory@gmail.com>
2026-03-20 00:26:17 -0500

    small update to esp32 code

--- esp32_cam/AINametag.ino
+++ esp32_cam/AINametag.ino
@@ -9,8 +9,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "SETUP-5A87";
-const char *password = "corral3010drink";
+const char *ssid = "*********";
+const char *password = "*********";
 
 void startCameraServer();
 void setupLedFlash();
